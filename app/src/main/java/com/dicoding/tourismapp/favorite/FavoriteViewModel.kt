669935266b775db02957113d1e1c0ad7ab37b493
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-01-26 17:07:09 +0700

    Codelab Dagger Hilt pada Tourism App

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -2,10 +2,11 @@ package com.dicoding.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class FavoriteViewModel @Inject constructor(tourismRepository: TourismUseCase) : ViewModel() {
 
     val favoriteTourism = tourismRepository.getFavoriteTourism().asLiveData()
--- app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/home/HomeViewModel.kt
@@ -2,10 +2,11 @@ package com.dicoding.tourismapp.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class HomeViewModel @Inject constructor(tourismRepository: TourismUseCase) : ViewModel() {
 
     val tourism = tourismRepository.getAllTourism().asLiveData()
